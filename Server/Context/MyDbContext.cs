fdb1c767e69d04e17f3ad4c1066bd387bc329c63
r3vi28 <116124448+r3vi28@users.noreply.github.com>
2023-02-18 02:04:40 -0400

    Definicion de la insfraestructura

---
+++ Server/Context/MyDbContext.cs
@@ -0,0 +1,56 @@
+using Microsoft.EntityFrameworkCore;
+using Restaurante.Server.Models;
+namespace Restaurante.Server.Context;
+
+public interface IMyDbContext
+{
+    DbSet<AsignacionMesa> AsignacionMesas { get; set; }
+    DbSet<Cliente> Clientes { get; set; }
+    DbSet<Factura> Facturas { get; set; }
+    DbSet<Mesa> Mesas { get; set; }
+    DbSet<MesaStatus> MesaStatuses { get; set; }
+    DbSet<MetodoPago> MetodoPagos { get; set; }
+    DbSet<Pedido> Pedidos { get; set; }
+    DbSet<PedidoStatus> PedidoStatuses { get; set; }
+    DbSet<Producto> Productos { get; set; }
+    DbSet<Reporte> Reportes { get; set; }
+    DbSet<Reservacion> Reservaciones { get; set; }
+    DbSet<RolUsuario> RolUsuarios { get; set; }
+    DbSet<Usuario> Usuarios { get; set; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
+
+public class MyDbContext : DbContext, IMyDbContext
+{
+    private readonly IConfiguration config;
+
+    public MyDbContext(IConfiguration _config)
+    {
+        config = _config;
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(config.GetConnectionString("Restaurante"));
+    }
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        return base.SaveChangesAsync(cancellationToken);
+    }
+
+    #region Tablas de mi base de datos
+    public DbSet<AsignacionMesa> AsignacionMesas { get; set; } = null!;
+    public DbSet<Cliente> Clientes { get; set; } = null!;
+    public DbSet<Factura> Facturas { get; set; } = null!;
+    public DbSet<Mesa> Mesas { get; set; } = null!;
+    public DbSet<MesaStatus> MesaStatuses { get; set; } = null!;
+    public DbSet<MetodoPago> MetodoPagos { get; set; } = null!;
+    public DbSet<Pedido> Pedidos { get; set; } = null!;
+    public DbSet<PedidoStatus> PedidoStatuses { get; set; } = null!;
+    public DbSet<Producto> Productos { get; set; } = null!;
+    public DbSet<Reporte> Reportes { get; set; } = null!;
+    public DbSet<Reservacion> Reservaciones { get; set; } = null!;
+    public DbSet<RolUsuario> RolUsuarios { get; set; } = null!;
+    public DbSet<Usuario> Usuarios { get; set; } = null!;
+    #endregion
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.ResponseCompression;
+using Restaurante.Server.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<MyDbContext>();
 
 var app = builder.Build();
 
